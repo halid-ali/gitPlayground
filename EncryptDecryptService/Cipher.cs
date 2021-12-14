aca85292d5dad021581d4af47aac7a2faf312ecd
Halid Ali <halid.ali@methodpark.de>
2021-12-14 14:39:44 +0100

    Move Cipher class into namespace

--- EncryptDecryptService/Cipher.cs
+++ EncryptDecryptService/Cipher.cs
@@ -1,41 +1,44 @@
 using System;
 using System.Security.Cryptography;
 
-public class Cipher
+namespace EncryptDecryptService
 {
-    public Cipher() : this(HashAlgorithm.SHA256)
-    { }
-
-    public Cipher(HashAlgorithm hashAlgorithm)
+    public class Cipher
     {
-        HashAlgorithm = hashAlgorithm;
-        SaltBuffer = CreateRandomByteBuffer();
-        InitBuffer = CreateRandomByteBuffer();
-    }
+        public Cipher() : this(HashAlgorithm.SHA256)
+        { }
 
-    public HashAlgorithm HashAlgorithm { get; }
+        public Cipher(HashAlgorithm hashAlgorithm)
+        {
+            HashAlgorithm = hashAlgorithm;
+            SaltBuffer = CreateRandomByteBuffer();
+            InitBuffer = CreateRandomByteBuffer();
+        }
 
-    private byte[] SaltBuffer { get; set; }
+        public HashAlgorithm HashAlgorithm { get; }
 
-    private byte[] InitBuffer { get; set; }
+        private byte[] SaltBuffer { get; set; }
 
-    public string EncryptText(string plainText, string password)
-    {
-        throw new NotImplementedException();
-    }
+        private byte[] InitBuffer { get; set; }
 
-    public string DecryptText(string encryptedText, string password)
-    {
-        throw new NotImplementedException();
-    }
+        public string EncryptText(string plainText, string password)
+        {
+            throw new NotImplementedException();
+        }
 
-    private byte[] CreateRandomByteBuffer()
-    {
-        //Generates a cryptographic random number
-        RNGCryptoServiceProvider cryptoServiceProvider = new RNGCryptoServiceProvider();
-        var buffer = new byte[Constants.InitSaltLength];
-        cryptoServiceProvider.GetBytes(buffer);
+        public string DecryptText(string encryptedText, string password)
+        {
+            throw new NotImplementedException();
+        }
 
-        return buffer;
+        private byte[] CreateRandomByteBuffer()
+        {
+            //Generates a cryptographic random number
+            RNGCryptoServiceProvider cryptoServiceProvider = new RNGCryptoServiceProvider();
+            var buffer = new byte[Constants.InitSaltLength];
+            cryptoServiceProvider.GetBytes(buffer);
+
+            return buffer;
+        }
     }
 }
