45ddcad93a3a1fc7700d09271e9fed1bfa56a859
Halid Ali <halid.ali@methodpark.de>
2021-12-14 14:13:29 +0100

    Create BaseEncryptBuffer class
    This class will be used for text and file encryptions
    Exclude .vscode from git history

---
+++ Buffers/BaseEncryptBuffer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text;
+
+namespace EncryptDecryptService.Buffers
+{
+    internal abstract class BaseEncryptBuffer
+    {
+        protected BaseEncryptBuffer(string password, byte[] initBuffer, byte[] saltBuffer, byte[] encryptedBuffer)
+        {
+            PasswordLength = password.Length;
+            PasswordBuffer = Encoding.UTF8.GetBytes(password);
+            InitBuffer = initBuffer;
+            SaltBuffer = saltBuffer;
+            EncryptedBufferLength = encryptedBuffer.Length;
+            EncryptedBuffer = encryptedBuffer;
+        }
+
+        protected int PasswordLength { get; set; }
+
+        protected byte[] PasswordBuffer { get; set; }
+
+        protected byte[] InitBuffer { get; set; }
+
+        protected byte[] SaltBuffer { get; set; }
+
+        protected int EncryptedBufferLength { get; set; }
+
+        protected byte[] EncryptedBuffer { get; set; }
+
+        protected byte[] CombinedBuffer { get; set; }
+
+        protected void CreateCombinedBuffer(int position)
+        {
+            var passwordLengthBuffer = BitConverter.GetBytes(PasswordLength);
+            var encryptedLengthBuffer = BitConverter.GetBytes(EncryptedBufferLength);
+
+            //write password length
+            Buffer.BlockCopy(passwordLengthBuffer, 0, CombinedBuffer, position, passwordLengthBuffer.Length);
+            position += passwordLengthBuffer.Length;
+
+            //write password buffer
+            Buffer.BlockCopy(PasswordBuffer, 0, CombinedBuffer, position, PasswordBuffer.Length);
+            position += PasswordBuffer.Length;
+
+            //write init buffer
+            Buffer.BlockCopy(InitBuffer, 0, CombinedBuffer, position, InitBuffer.Length);
+            position += InitBuffer.Length;
+
+            //write salt buffer
+            Buffer.BlockCopy(SaltBuffer, 0, CombinedBuffer, position, SaltBuffer.Length);
+            position += SaltBuffer.Length;
+
+            //write encrypted buffer length
+            Buffer.BlockCopy(encryptedLengthBuffer, 0, CombinedBuffer, position, encryptedLengthBuffer.Length);
+            position += encryptedLengthBuffer.Length;
+
+            //write encrypted buffer
+            Buffer.BlockCopy(EncryptedBuffer, 0, CombinedBuffer, position, EncryptedBuffer.Length);
+        }
+    }
+}
