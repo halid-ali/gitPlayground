2cc75e8ce04f3acd99b622c9626b5f315697c503
Halid Ali <halid.ali@methodpark.de>
2021-12-15 13:08:29 +0100

    Call Decrypt from main

--- Program.cs
+++ Program.cs
@@ -12,12 +12,20 @@ namespace gitPlayground
         {
             var arguments = ArgumentParser.Parse(args);
 
-            if (arguments != null && arguments.IsEncrypt) Encrypt(arguments);
+            if (arguments != null && arguments.IsEncrypt)
+                Console.WriteLine(Encrypt(arguments));
+            else
+                Console.WriteLine(Decrypt(arguments));
         }
 
         private static string Encrypt(Arguments args)
         {
             return cipher.EncryptText(args.InputText, args.Password);
+        }
+
+        private static string Decrypt(Arguments args)
+        {
+            return cipher.DecryptText(args.InputText, args.Password);
         }
     }
 }
